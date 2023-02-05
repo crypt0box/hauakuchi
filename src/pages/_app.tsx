ee0a3a56910783a6fe457a479631b917bd2de697
cryptobox <pray,happy76@gmail.com>
2023-02-05 21:06:57 +0900

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Nico Moji",
+    body: "Noto Sans JP",
+  },
+  styles: {
+    global: {
+      body: {
+        color: "tomato",
+        backgroundColor: "orange.50",
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>めっちゃ早口でしゃべりたい</title>
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ChakraProvider>
+    </>
+  );
 }
