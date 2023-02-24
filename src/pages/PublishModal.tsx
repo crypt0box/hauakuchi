cb45b66f049a77e5b973c0b903d7a51c014f5c2c
cryptobox <pray,happy76@gmail.com>
2023-02-24 22:47:17 +0900

    feat: emojiPickerを仮で実装

--- src/pages/PublishModal.tsx
+++ src/pages/PublishModal.tsx
@@ -5,33 +5,54 @@ import {
   ModalCloseButton,
   ModalContent,
   ModalFooter,
-  ModalHeader,
   ModalOverlay,
   ModalProps,
 } from "@chakra-ui/react";
+import { EmojiClickData, EmojiStyle } from "emoji-picker-react";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+const Picker = dynamic(() => import("emoji-picker-react"), { ssr: false });
 
 type PublishModalProps = Omit<ModalProps, "children">;
+type EmojiData = Pick<EmojiClickData, "unified" | "emoji">;
+
+const initialEmoji: EmojiData = {
+  emoji: "🙂",
+  unified: "1f642",
+};
 
 export const PublishModal: React.FC<PublishModalProps> = ({
   isOpen,
   onClose,
-}) => (
-  <Modal isOpen={isOpen} onClose={onClose} isCentered>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalCloseButton />
-      <ModalBody mt="10">aaa</ModalBody>
-      <ModalFooter justifyContent="center">
-        <Button
-          backgroundColor="tomato"
-          color="orange.50"
-          _hover={{ backgroundColor: "#FF7860" }}
-          mr={3}
-          onClick={onClose}
-        >
-          投稿する
-        </Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-);
+}) => {
+  const [chosenEmoji, setChosenEmoji] = useState<EmojiData>(initialEmoji);
+  console.log("🚀 ~ file: PublishModal.tsx:29 ~ chosenEmoji:", chosenEmoji);
+
+  const onEmojiClick = (emojiObject: EmojiClickData) => {
+    const { emoji, unified } = emojiObject;
+    setChosenEmoji({ emoji, unified });
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton />
+        <ModalBody mt="10">
+          <Picker onEmojiClick={onEmojiClick} emojiStyle={EmojiStyle.TWITTER} />
+        </ModalBody>
+        <ModalFooter justifyContent="center">
+          <Button
+            backgroundColor="tomato"
+            color="orange.50"
+            _hover={{ backgroundColor: "#FF7860" }}
+            mr={3}
+            onClick={onClose}
+          >
+            投稿する
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
