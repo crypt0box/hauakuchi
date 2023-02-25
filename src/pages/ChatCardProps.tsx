44721776e0364d132097025a2fa4e4770cca52be
cryptobox <pray,happy76@gmail.com>
2023-02-25 23:29:29 +0900

    fix: アニメーションの修正

--- src/pages/ChatCardProps.tsx
+++ src/pages/ChatCardProps.tsx
@@ -13,8 +13,8 @@ import {
   useDisclosure,
   Image as ChakraImage,
   Text,
-  keyframes,
   useMediaQuery,
+  Box,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -29,7 +29,7 @@ export const ChatCard: React.FC<ChatCardProps> = ({ data, ...rest }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [counter, setCounter] = useState(0);
   const [displayMessages, setDisplayMessages] = useState<string[]>([]);
-  const splitMessage = splitString(data.message, isLargerThan600 ? 24 : 18);
+  const splitMessage = splitString(data.message, isLargerThan600 ? 23 : 18);
 
   useEffect(() => {
     if (!splitMessage) return;
@@ -64,22 +64,56 @@ export const ChatCard: React.FC<ChatCardProps> = ({ data, ...rest }) => {
         <ModalOverlay />
         <ModalContent mx={{ base: "4" }}>
           <ModalCloseButton />
-          <ModalBody mt="8" mx="auto" pb="0" justifyContent="center">
-            {displayMessages.length !== 0 &&
-              displayMessages.map((message, index) => (
-                <Text key={index}>{message}</Text>
-              ))}
-            {splitMessage && splitMessage[counter] && (
-              <TypeAnimation
-                key={counter}
-                sequence={[
-                  splitMessage[counter],
-                  () => setCounter((prev) => prev + 1),
-                ]}
-                wrapper="p"
-                cursor={true}
+          <ModalBody mt="8" pb="0">
+            <Flex mb="4" justifyContent="center">
+              <Image
+                src={`https://twemoji.maxcdn.com/v/latest/svg/${data.icon}.svg`}
+                alt={`感情アイコン_${data.icon}`}
+                width={32}
+                height={32}
               />
-            )}
+            </Flex>
+            <Box
+              mx="auto"
+              position="relative"
+              display="inline-block"
+              p="7px 10px"
+              w="full"
+              color="#555"
+              background="#e0edff"
+              borderRadius="8px"
+              justifyContent="center"
+              _before={{
+                content: '""',
+                position: "absolute",
+                top: "-28px",
+                left: "50%",
+                ml: "-15px",
+                border: "15px solid transparent",
+                borderBottom: "15px solid #e0edff",
+              }}
+            >
+              {displayMessages.length !== 0 &&
+                displayMessages.map((message, index) => (
+                  <Text key={index} textAlign="center">
+                    {message}
+                  </Text>
+                ))}
+              {splitMessage && splitMessage[counter] && (
+                <TypeAnimation
+                  key={counter}
+                  sequence={[
+                    splitMessage[counter],
+                    () => setCounter((prev) => prev + 1),
+                  ]}
+                  wrapper="p"
+                  cursor={true}
+                  style={{
+                    textAlign: "center",
+                  }}
+                />
+              )}
+            </Box>
           </ModalBody>
           <ModalFooter>
             <Button
