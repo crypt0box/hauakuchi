05ebd1d110941755bfb9ea57cce944cbb346de43
cryptobox <pray,happy76@gmail.com>
2023-02-26 15:24:48 +0900

    feat: 全てのアニメーション終了時再度アニメイトするよう実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import { Box } from "@chakra-ui/react";
 import { useFetchMessages } from "@/hooks/useFetchMessages";
 import { ChatCard } from "./ChatCard";
 import { motion, useAnimationControls } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
   const { res } = useFetchMessages();
   const controls = useAnimationControls();
 
-  useEffect(() => {
+  const startAnimation = () =>
     controls.start((i) => {
       const randInt = Math.random() * 2;
       return {
@@ -23,6 +23,9 @@ export default function Home() {
         },
       };
     });
+
+  useEffect(() => {
+    startAnimation();
   }, [controls, res]);
 
   return (
@@ -32,7 +35,14 @@ export default function Home() {
         <Box position="relative" h="full">
           {res &&
             res.map((el, index) => (
-              <motion.div key={el.id} custom={index} animate={controls}>
+              <motion.div
+                key={el.id}
+                custom={index}
+                animate={controls}
+                onAnimationComplete={() =>
+                  index === res.length - 1 && startAnimation()
+                }
+              >
                 <ChatCard
                   position="absolute"
                   top={`${Math.floor(Math.random() * 80)}%`}
