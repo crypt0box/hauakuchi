78d36aafdedd10b6e17781151df56ac068471798
cryptobox <pray,happy76@gmail.com>
2023-02-26 14:37:06 +0900

    feat: 早口アニメーション実装

--- src/pages/Title.tsx
+++ src/pages/Title.tsx
@@ -1,15 +1,45 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 
-export const Title = () => (
-  <Box color="tomato" textAlign="center">
-    <Heading as="h1" size="xl" my="4">
-      めっちゃはやくちでしゃべりたい
-    </Heading>
-    <Text size="lg" textAlign="center" fontFamily="Noto Sans JP">
-      つい早口になっちゃうくらい
-    </Text>
-    <Text size="lg" textAlign="center" fontFamily="Noto Sans JP">
-      しゃべりたいことを投稿しよう
-    </Text>
-  </Box>
-);
+export const Title = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <Box color="tomato" textAlign="center">
+      <Heading as="h1" size="xl" my="4">
+        <TypeAnimation
+          sequence={[
+            "めっちゃはやくちでしゃべりたい",
+            () => setCounter((prev) => prev + 1),
+          ]}
+          wrapper="span"
+          cursor={false}
+        />
+      </Heading>
+      <Text size="lg" textAlign="center" fontFamily="Noto Sans JP">
+        {counter > 0 && (
+          <TypeAnimation
+            sequence={[
+              "つい早口になっちゃうくらい",
+              () => setCounter((prev) => prev + 1),
+            ]}
+            wrapper="span"
+            cursor={false}
+          />
+        )}
+      </Text>
+      <Text size="lg" textAlign="center" fontFamily="Noto Sans JP">
+        {counter > 1 && (
+          <TypeAnimation
+            sequence={[
+              "しゃべりたいことを投稿しよう",
+              () => setCounter((prev) => prev + 1),
+            ]}
+            wrapper="span"
+            cursor={false}
+          />
+        )}
+      </Text>
+    </Box>
+  );
+};
