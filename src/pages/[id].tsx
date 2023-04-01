014d58d7072d5abdebf53064e902e93dc04560a9
cryptobox <pray,happy76@gmail.com>
2023-04-01 21:43:25 +0900

    feat: ogp情報を設置

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -15,6 +15,7 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -34,73 +35,81 @@ export default function MessageModal({ data }: { data: MessageResponse }) {
   }, [counter]);
 
   return (
-    <Modal isOpen={true} onClose={onCloseDialog}>
-      <ModalOverlay />
-      <ModalContent mx={{ base: "4" }}>
-        <ModalCloseButton />
-        <ModalBody mt="8" pb="0">
-          <Flex mb="4" justifyContent="center">
-            <Image
-              src={`https://twemoji.maxcdn.com/v/latest/svg/${data.icon}.svg`}
-              alt={`感情アイコン_${data.icon}`}
-              width={32}
-              height={32}
-            />
-          </Flex>
-          <Box
-            mx="auto"
-            position="relative"
-            display="inline-block"
-            p="7px 10px"
-            w="full"
-            color="#555"
-            background="#e0edff"
-            borderRadius="8px"
-            justifyContent="center"
-            _before={{
-              content: '""',
-              position: "absolute",
-              top: "-28px",
-              left: "50%",
-              ml: "-15px",
-              border: "15px solid transparent",
-              borderBottom: "15px solid #e0edff",
-            }}
-          >
-            {displayMessages.length !== 0 &&
-              displayMessages.map((message, index) => (
-                <Text key={index} textAlign="center">
-                  {message}
-                </Text>
-              ))}
-            {splitMessage && splitMessage[counter] && (
-              <TypeAnimation
-                key={counter}
-                sequence={[
-                  splitMessage[counter],
-                  () => setCounter((prev) => prev + 1),
-                ]}
-                wrapper="p"
-                cursor={true}
-                style={{
-                  textAlign: "center",
-                }}
+    <>
+      <Head>
+        <meta
+          property="og:image"
+          content={`${process.env.NEXT_PUBLIC_OGP_BASEURL}/api/og?title=${data.message}`}
+        />
+      </Head>
+      <Modal isOpen={true} onClose={onCloseDialog}>
+        <ModalOverlay />
+        <ModalContent mx={{ base: "4" }}>
+          <ModalCloseButton />
+          <ModalBody mt="8" pb="0">
+            <Flex mb="4" justifyContent="center">
+              <Image
+                src={`https://twemoji.maxcdn.com/v/latest/svg/${data.icon}.svg`}
+                alt={`感情アイコン_${data.icon}`}
+                width={32}
+                height={32}
               />
-            )}
-          </Box>
-        </ModalBody>
-        <ModalFooter>
-          <Button
-            colorScheme="twitter"
-            w="full"
-            onClick={onCloseDialog}
-            leftIcon={<ChakraImage w="6" h="6" src="/twitter.svg" />}
-          >
-            Twitterでシェアする
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+            </Flex>
+            <Box
+              mx="auto"
+              position="relative"
+              display="inline-block"
+              p="7px 10px"
+              w="full"
+              color="#555"
+              background="#e0edff"
+              borderRadius="8px"
+              justifyContent="center"
+              _before={{
+                content: '""',
+                position: "absolute",
+                top: "-28px",
+                left: "50%",
+                ml: "-15px",
+                border: "15px solid transparent",
+                borderBottom: "15px solid #e0edff",
+              }}
+            >
+              {displayMessages.length !== 0 &&
+                displayMessages.map((message, index) => (
+                  <Text key={index} textAlign="center">
+                    {message}
+                  </Text>
+                ))}
+              {splitMessage && splitMessage[counter] && (
+                <TypeAnimation
+                  key={counter}
+                  sequence={[
+                    splitMessage[counter],
+                    () => setCounter((prev) => prev + 1),
+                  ]}
+                  wrapper="p"
+                  cursor={true}
+                  style={{
+                    textAlign: "center",
+                  }}
+                />
+              )}
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              colorScheme="twitter"
+              w="full"
+              onClick={onCloseDialog}
+              leftIcon={<ChakraImage w="6" h="6" src="/twitter.svg" />}
+            >
+              Twitterでシェアする
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
